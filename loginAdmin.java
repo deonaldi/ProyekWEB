adc1b2c8e9d8bcaf433439507de7198f3ea1603b
vinsensiusverdi <daokdalam@gmail.com>
2016-06-08 14:32:45 +0700

    Add files via upload

---
+++ loginAdmin.java
@@ -0,0 +1,72 @@
+package controller;
+
+import Model.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet(name = "Login", urlPatterns = {"/Login"})
+public class loginAdmin extends HttpServlet {
+DatabaseManager m = new DatabaseManager();
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher;       
+        Mahasiswa mahasiswa = null;
+        Admin admin = null;
+        String username = request.getParameter("username");
+        String pass = request.getParameter("pass");
+        HttpSession session = request.getSession();
+//        try{
+//            Integer.getInteger(nim.substring(0, 3));
+//        }
+//        catch(Exception ec){
+            try{
+            mahasiswa = m.login(username, pass);
+            }
+            catch(Exception e){
+                 String ex = e.getMessage();
+                 tampil(response, ex);
+            }
+//        }
+        //Login Admin
+//        try{
+//            admin = m.loginAdmin(nim, pass);
+//            }
+//            catch(Exception e){
+//                 String ex = e.getMessage();
+//                 tampil(response, ex);
+//            }
+          
+        session.setAttribute("admin", admin);
+        dispatcher = request.getRequestDispatcher("admin.jsp");
+        dispatcher.forward(request, response);   
+        
+    }
+    
+    void tampil(HttpServletResponse response, String pesan) throws IOException{
+        PrintWriter out = response.getWriter();
+        try {
+                    out.println("<!DOCTYPE html>");
+                    out.println("<html>");
+                    out.println("<head>");
+                    out.println("<title>ERROR</title>");            
+                    out.println("</head>");
+                    out.println("<body>");
+                    out.println("<h1>"+pesan+"</h1>");
+                    out.println("<a href=\"index.jsp\">Login</a>");
+                    out.println("</body>");
+                    out.println("</html>");
+                    }      
+        finally {
+            out.close();
+        }
+    }
+}
